5aa565e3c45988805fdbc7340314a3beaa7ba1f8
Nadia <nadialazo118@gmail.com>
2019-06-16 11:20:19 -0400

    version02

--- app.js
+++ app.js
@@ -32,25 +32,39 @@ let spf = [
         "None", "Neutrogena Hydro Boost Gel Lotion Sunscreen SPF 30", "Avene High Protection Tinted Compact SPF 50", "La Roche-Posay Anthelios AOX Daily Antioxidant Serum with Sunscreen SPF 50", "Cetaphil PRO Oil Absorbing Moisturizer SPF 30", "EltaMD UV Clear Broad Spectrum SPF 46", "Clinique Super City Block Oil-Free Daily Face Protector 40 SPF", "Colorescience Sunforgettable Brush-on Sunscreen SPF 50" 
 ];
 
-
+let userSchema = new mongoose.Schema({
+        fullName: String,
+        email: String,
+        userName: String,
+        faceWash: String,
+        toner: String,
+        serum: String,
+        moisturizer: String
+        });
+        
+        let user = mongoose.model('user', userSchema);
 
 // Port
 const PORT = process.env.PORT || 3000;
 
 //Middleware
 
+//include express modules
 app.set('view engine', 'ejs');
 app.use("/html", express.static('html'));
 app.use(bodyParser.json());
-app.use("/js", express.static("js"));
+app.use(bodyParser.urlencoded({ extended: false }))
 
+//set static page directories
+app.use("/js", express.static("js"));
 app.use("/css", express.static("css"))
 
-
+//MAIN ROUTE
 app.get('/', (req, res) => {
         res.render('index.ejs');
           });
 
+// MEMBER SIGN UP FORM ROUTE
 app.get('/newMember', (req, res)=> {
         res.render('memberSignUp', { 
                 faceWash: faceWash,
@@ -61,53 +75,53 @@ app.get('/newMember', (req, res)=> {
         });
 });
 
+//  REGISTRATION ROUTE
 app.get('/thankYou', (req, res) => {
-        res.render('registrationMessage', {
+        res.render('registrationMessage.ejs', {
                 username:username,
         })
 });
 
-app.get('/day_routine', (req, res) => {
-        res.render('day.ejs')
-});
 
+//SINK CARE ROUTINE ROUTES
 app.get('/night_routine', (req, res) => {
         res.render('night.ejs')
 });
+app.post('/newMember', function(req, res){
+        console.log(req.body);
+        new user({
+                        fullName: req.body.fullName,
+                        email: req.body.email,
+                        userName: req.body.userName,
+                        faceWash: req.body.faceWash,
+                        toner: req.body.toner,
+                        serum: req.body.serum,
+                        moisturizer:req.body.moisturizer    
+        }).save();
+        res.redirect('/thankYou')
+        
+})
+
+app.get('/day_routine', (req, res) => { 
+        //   Get all companies from the database
+        user.find({
+                username: username,
+                faceWash: faceWash,
+                toner: toner,
+                serum: serum,
+                moisturizer: moisturizer,
+                spf: spf,
+        }, (err, allUsers) => {
+          if(err){
+            console.log('THERE WAS AN ERROR: ' + err);
+          } else {
+            res.render('day', {users: allUsers});
+          }
+        });
+      });
 
 
-
-
-
-// let userSchema = new mongoose.userSchema({
-// fullName: String,
-// _ID= String,
-// userName: String,
-// faceWash: String,
-// toner: String,
-// serum: String,
-// moisturizer: String
-// });
-
-// let user = mongoose.model('user', userSchema);
-
-// app.post('/newUser', (req, res) => {
-//         new user({
-//                 fullName: fullName,
-//                 _ID: req.body.email,
-//                 userName: req.body.userName,
-//                 faceWash: req.body.faceWash,
-//                 toner: req.body.toner,
-//                 serum: req.body.serum,
-//                 moisturizer:req.body.moisturizer
-//         }).save((err, doc) => {
-//                 if(err) res.json(err);
-//                 else res.render('/thankYou')
-//         })
-// })
-
-
-
-
-
+      app.get('/day_routine', (req, res) => {
+        res.render('day.ejs', { user: user })
+});
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
